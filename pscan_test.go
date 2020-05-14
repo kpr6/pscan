0bf59d9d381ca05b96a05123f2adf8938f4af060
Pranaye <pranaykarnati006@gmail.com>
2020-05-14 23:53:30 +0530

    added dummy test

--- pscan_test.go
+++ pscan_test.go
@@ -1,3 +1,12 @@
-func Test() {
+package main
 
+import "testing"
+
+func TestDummy(t *testing.T) {
+	want := "hello"
+	got := "hello"
+
+	if got != want {
+		t.Errorf("got %q want %q \n", got, want)
+	}
 }
